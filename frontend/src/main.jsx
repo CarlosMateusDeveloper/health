bf78a3b949d7e760dc3def1abd7b6435a4070849
CarlosMateusDeveloper <mateusdev33@gmail.com>
2026-07-01 22:12:07 -0300

    fix: react components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
@@ -8,3 +11,4 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
